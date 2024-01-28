aade9856b69b8b2a0948046253c7a5470d8fd8a8
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-01-28 23:06:28 +0100

    feat: configure environment variables for GitHub integration in next.config.mjs
    - Add `GITHUB_CLIENT_ID` and `GITHUB_CLIENT_SECRET` to the `env` configuration in `next.config.mjs`
    - This enables the use of GitHub OAuth credentials within the Next.js application

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+  }
+};
 
 export default nextConfig;
