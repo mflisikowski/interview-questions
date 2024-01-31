0174e044d8657b72a5865400a2603f75faa9c630
Mateusz Flisikowski <contact@mflisikowski.dev>
2024-01-31 09:37:40 +0100

    feat: Add Apple ID authentication provider
    - Introduced Apple ID as an authentication provider in `auth.ts`.
    - Configured `Apple` provider from `next-auth/providers/apple` with environment variables `APPLE_CLIENT_ID` and `APPLE_CLIENT_SECRET`.

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth, { type NextAuthConfig } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import GitHub from "next-auth/providers/github";
+import Apple from "next-auth/providers/apple";
 import { prisma } from "@/prisma";
 
 export const authOptions: NextAuthConfig = {
@@ -21,6 +22,10 @@ export const authOptions: NextAuthConfig = {
     GitHub( {
       clientId: process.env.GITHUB_CLIENT_ID ?? '',
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+    } ),
+    Apple( {
+      clientId: process.env.APPLE_CLIENT_ID ?? '',
+      clientSecret: process.env.APPLE_CLIENT_SECRET ?? '',
     } ),
   ],
 
